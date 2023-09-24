4432ec08899e4e6444a366cc51f2351b470dc232
Robiul72 <robiulislam366972@gmail.com>
2023-09-25 04:15:05 +0600

    add
    25-09-23
    final full project

---
+++ Final_Full_Project/src/final_full_project/Final_Full_Project.java
@@ -0,0 +1,14 @@
+
+package final_full_project;
+
+import view.LoginPage;
+
+public class Final_Full_Project {
+
+    public static void main(String[] args) {
+        LoginPage fo = new LoginPage();
+        fo.setLocationRelativeTo(null);
+        fo.setVisible(true);
+    }
+    
+}
